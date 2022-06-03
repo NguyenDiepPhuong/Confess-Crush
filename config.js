f0389adf70a9e2b4559cff92c5fd3eed3c21679f
NguyenDiepPhuong <106795075+NguyenDiepPhuong@users.noreply.github.com>
2022-06-03 15:46:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "mik rất thik cậu đó >3",
+    introTitle: 'Bạn cute ơi',
+    introDesc: `Em đây rất thik đồng hồ
+    Nhưng vẫn thik nhất làm bồ của anh`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Không đâu nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
